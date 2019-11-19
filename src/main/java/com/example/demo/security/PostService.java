3ba7b04614074f35f5dd131a97b488491182bd01
Pankti Bardolia <pbardolia@albany.edu>
2019-11-18 23:37:18 -0500

    Second Commit second day

---
+++ src/main/java/com/example/demo/security/PostService.java
@@ -0,0 +1,63 @@
+package com.example.demo.security;
+
+import com.example.demo.dto.PostDto;
+import com.example.demo.exception.PostNotFoundException;
+import com.example.demo.model.post;
+import com.example.demo.repository.PostRepository;
+import com.example.demo.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private AuthService authService;
+    @Autowired
+    private PostRepository postRepository;
+
+    @Transactional
+    public List<PostDto> showAllPosts() {
+        List<post> posts = postRepository.findAll();
+        return posts.stream().map(this::mapFromPostToDto).collect(toList());
+    }
+
+    @Transactional
+    public void createPost(PostDto postDto) {
+        post post = mapFromDtoToPost(postDto);
+        postRepository.save(post);
+    }
+
+    @Transactional
+    public PostDto readSinglePost(Long id) {
+        post post = postRepository.findById(id).orElseThrow(() -> new PostNotFoundException("For id " + id));
+        return mapFromPostToDto(post);
+    }
+
+    private PostDto mapFromPostToDto(post post) {
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setContent(post.getContent());
+        postDto.setUsername(post.getUsername());
+        return postDto;
+    }
+
+    private post mapFromDtoToPost(PostDto postDto) {
+        post post = new post();
+        post.setTitle(postDto.getTitle());
+        post.setContent(postDto.getContent());
+        User loggedInUser = authService.getCurrentUser().orElseThrow(() -> new IllegalArgumentException("User Not Found"));
+        post.setCreatedOn(Instant.now());
+        post.setUsername(loggedInUser.getUsername());
+        post.setUpdatedOn(Instant.now());
+        return post;
+    }
+}
---
+++ src/main/java/com/example/demo/service/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String authenticationToken;
+    private String username;
+
+    AuthenticationResponse(String authenticationToken, String username){
+
+    }
+}
